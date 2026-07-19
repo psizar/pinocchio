29d9e4742c6c079ceffbe557c1269116fecde90d
Siddhanth B N <bnsiddhanth0@gmail.com>
2026-07-19 23:04:44 +0530

    CUR-24: add integration tests

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Io = std.Io;
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
@@ -43,4 +44,69 @@ pub fn build(b: *std.Build) void {
     const test_cmd = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&test_cmd.step);
+
+    const int_tests_cmd = b.step("test-integration", "Run Integration Tests");
+
+    const int_tests_step = b.allocator.create(IntegrationTestStep) catch @panic("OOM");
+    int_tests_step.* = IntegrationTestStep{
+        .step = std.Build.Step.init(.{
+            .id = .custom,
+            .name = "integration-test",
+            .owner = b,
+            .makeFn = makeIntegrationTest,
+        }),
+        .exe = exe,
+    };
+
+    int_tests_step.step.dependOn(&exe.step);
+    int_tests_cmd.dependOn(&int_tests_step.step);
+}
+
+const IntegrationTestStep = struct {
+    step: std.Build.Step,
+    exe: *std.Build.Step.Compile,
+};
+
+fn makeIntegrationTest(step: *std.Build.Step, options: std.Build.Step.MakeOptions) !void {
+    _ = options;
+
+    const self: *IntegrationTestStep = @fieldParentPtr("step", step);
+
+    const gpa = step.owner.allocator;
+    const io = step.owner.graph.io;
+    const server_path = self.exe.generated_bin.?.getPath();
+
+    var child = try std.process.spawn(io, .{
+        .argv = &.{server_path},
+        .stdin = .ignore,
+        .stdout = .pipe,
+        .stderr = .pipe,
+    });
+
+    defer child.kill(io);
+
+    var mr_buf: Io.File.MultiReader.Buffer(2) = undefined;
+    var mr: Io.File.MultiReader = undefined;
+    mr.init(gpa, io, mr_buf.toStreams(), &.{ child.stdout.?, child.stderr.? });
+    defer mr.deinit();
+
+    const timeout: Io.Timeout = .{ .duration = .{
+        .raw = Io.Duration.fromSeconds(1),
+        .clock = .awake,
+    } };
+
+    while (mr.fill(4096, timeout)) |_| {} else |err| switch (err) {
+        error.Timeout => {},
+        error.EndOfStream => {},
+        else => |e| return e,
+    }
+
+    const output = try mr.toOwnedSlice(1);
+    defer gpa.free(output);
+
+    const init_str = "Hello from Pinocchio Guest (PID 1)!";
+
+    if (std.mem.find(u8, output, init_str) == null) {
+        return step.fail("expected init string: {s}, got: {s}", .{ init_str, output });
+    }
 }
